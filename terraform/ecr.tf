4768fd46b95df699bcc77412e452a380a8f409eb
XxxKMSxxX <kms1342152@gmail.com>
2024-07-13 11:46:57 +0900

    一度全部消す

--- terraform/ecr.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "collector" {
-  name = "${local.project_name}-ecr-repo"
-}
--- terraform/kinesis.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_kinesis_stream" "my_stream" {
-  name        = "${local.project_name}-kinesis-stream"
-  shard_count = 1
-}
