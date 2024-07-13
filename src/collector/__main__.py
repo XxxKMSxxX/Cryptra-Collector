aed355416b3788f28e07cb0a8be9390be4a626ac
XxxKMSxxX <kms1342152@gmail.com>
2024-07-13 10:53:42 +0900

    ヘルスチェックエンドポイントを追加

--- src/collector/__main__.py
+++ src/collector/__main__.py
@@ -9,10 +9,27 @@ import asyncio
 import pybotters
 import signal
 import traceback
+from threading import Thread
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+first_message_received = False
+
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    global first_message_received
+    if first_message_received:
+        return jsonify(status="OK")
+    else:
+        return jsonify(status="Waiting for first message")
+
+
+def start_flask_app():
+    app.run(host='0.0.0.0', port=8080)
 
 
 async def main(args: Namespace) -> None:
-
     KINESIS_STREAM_NAME = (
         f"{args.exchange}-{args.contract}-{args.symbol.upper()}"
     )
@@ -20,11 +37,18 @@ async def main(args: Namespace) -> None:
     kinesis = Kinesis(args.aws_region)
 
     def handler(msg: Any, ws: ClientWebSocketResponse) -> None:
+        global first_message_received
+        if not first_message_received:
+            first_message_received = True
         messages = exchange.on_message(msg)
         if messages:
             asyncio.create_task(
                 kinesis.publish(KINESIS_STREAM_NAME, messages)
             )
+            print(messages, flush=True)
+
+    flask_thread = Thread(target=start_flask_app)
+    flask_thread.start()
 
     async with pybotters.Client() as client:
         ws = await client.ws_connect(
