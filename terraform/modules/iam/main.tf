d666039a79149d2115ab8d140edc6d12e943e4f3
XxxKMSxxX <kms1342152@gmail.com>
2024-07-13 16:14:18 +0900

    いろいろ修正

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,18 +1,64 @@
-data "aws_iam_policy" "existing_execution_policy" {
-  name = "github-actions-execution-policy"
-}
-
 resource "aws_iam_policy" "execution_policy" {
-  count       = length(data.aws_iam_policy.existing_execution_policy.arn) == 0 ? 1 : 0
   name        = "github-actions-execution-policy"
   description = "Policy to allow AWS operations for github-actions user"
-  policy      = file("${path.module}/policy.json")
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "ECRPermissions",
+        Effect = "Allow",
+        Action = [
+          "ecr:CreateRepository",
+          "ecr:DeleteRepository",
+          "ecr:DescribeRepositories",
+          "ecr:ListTagsForResource",
+          "ecr:PutImageScanningConfiguration",
+          "ecr:CompleteLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:InitiateLayerUpload",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchDeleteImage",
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:ListImages",
+          "ecr:PutImage",
+          "ecr:PutLifecyclePolicy",
+          "ecr:GetLifecyclePolicy",
+          "ecr:DeleteLifecyclePolicy"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid = "ECRPublicPermissions",
+        Effect = "Allow",
+        Action = [
+          "ecr-public:CreateRepository",
+          "ecr-public:DeleteRepository",
+          "ecr-public:DescribeRepositories",
+          "ecr-public:ListTagsForResource",
+          "ecr-public:PutImageScanningConfiguration",
+          "ecr-public:CompleteLayerUpload",
+          "ecr-public:UploadLayerPart",
+          "ecr-public:InitiateLayerUpload",
+          "ecr-public:BatchCheckLayerAvailability",
+          "ecr-public:BatchDeleteImage",
+          "ecr-public:BatchGetImage",
+          "ecr-public:GetDownloadUrlForLayer",
+          "ecr-public:GetRepositoryPolicy",
+          "ecr-public:ListImages",
+          "ecr-public:PutImage",
+          "ecr-public:PutLifecyclePolicy",
+          "ecr-public:GetLifecyclePolicy",
+          "ecr-public:DeleteLifecyclePolicy"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_user_policy_attachment" "github_actions_policy_attachment" {
   user       = "github-actions"
-  policy_arn = coalesce(
-    length(data.aws_iam_policy.existing_execution_policy.arn) == 0 ? null : data.aws_iam_policy.existing_execution_policy.arn,
-    aws_iam_policy.execution_policy[0].arn
-  )
+  policy_arn = aws_iam_policy.execution_policy.arn
 }
