0f570e4091ec5b4f4f35f1266e60e1885ab96f1e
XxxKMSxxX <kms1342152@gmail.com>
2024-07-13 14:11:58 +0900

    policyの読込修正

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -3,14 +3,16 @@ data "aws_iam_policy" "existing_execution_policy" {
 }
 
 resource "aws_iam_policy" "execution_policy" {
+  count       = length(data.aws_iam_policy.existing_execution_policy.arn) == 0 ? 1 : 0
   name        = "github-actions-execution-policy"
   description = "Policy to allow AWS operations for github-actions user"
   policy      = file("${path.module}/policy.json")
-
-  count = length(data.aws_iam_policy.existing_execution_policy.arn) == 0 ? 1 : 0
 }
 
 resource "aws_iam_user_policy_attachment" "github_actions_policy_attachment" {
   user       = "github-actions"
-  policy_arn = length(data.aws_iam_policy.existing_execution_policy.arn) == 0 ? aws_iam_policy.execution_policy[0].arn : data.aws_iam_policy.existing_execution_policy.arn
+  policy_arn = coalesce(
+    length(data.aws_iam_policy.existing_execution_policy.arn) == 0 ? null : data.aws_iam_policy.existing_execution_policy.arn,
+    aws_iam_policy.execution_policy[0].arn
+  )
 }
